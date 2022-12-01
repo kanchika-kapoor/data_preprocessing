b55e656741bf0cca95eabc36854c4a8f1fcd4c26
kanchika-kapoor <kanchika.ai@gmail.com>
2022-12-02 04:37:56 +0530

    change the source of data

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import json
 from io import StringIO
 
 # specify headers
-header = {'content':'application/json'}
+header = {
+    'content':'application/json', 
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
+}
+
 # specify url
-url = 'https://killedbygoogle.com/_next/data/kwu8voS3XHNDh6ya51_rK/index.json'
+url = 'https://ycharts.com/events/events_data/json?date=12/01/2022&startDate=&endDate=&pageNum=5&eventGroups=earnings,dividends,splits_spinoffs,other&securitylistName=all_stocks&securityGroup=company&sort=asc'
 # call get request
 k = requests.get(url, headers = header)
 # get relevant data from response json
-data = k.json()['pageProps']['items']
+data = k.json()['events']
 # create dataframe from json
 # newer version of pandas expects buffer of
 # stringified json for read_json
